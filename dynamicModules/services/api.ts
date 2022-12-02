92e9ae77d54cf57e39cf64ee543a6c11dba74aba
liuxw <xiaowei.liu0812@outlook.com>
2022-12-02 10:19:10 +0800

    feat: [server] 往/server发请求,而不是:9011

--- dynamicModules/services/api.ts
+++ dynamicModules/services/api.ts
@@ -1,2 +1,5 @@
 /* 接口的 host */
-export const API_HOST = `//${window.location.hostname}:9011`;
+// export const API_HOST = `//${window.location.hostname}:9011`;
+
+// 部署后，往/server发的请求，会转发到9011上
+export const API_HOST = `//${window.location.hostname}/server`;
